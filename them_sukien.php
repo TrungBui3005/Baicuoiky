92b354d1428a378ed80a31fe55497bef7d54c474
Trung Bui <buitrung3005@gmail.com>
2025-12-31 01:41:18 +0700

    Sua mot so loi nho

--- them_sukien.php
+++ them_sukien.php
@@ -3,6 +3,14 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
+// Kiểm tra nếu chưa đăng nhập HOẶC không phải admin thì đuổi ra trang chủ
+if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
+    echo "<script>
+            alert('Bạn không có quyền truy cập trang quản trị!');
+            window.location.href='index.php';
+          </script>";
+    exit();
+}
 ?>
 <?php 
 include 'connect.php'; 
